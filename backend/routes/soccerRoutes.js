d30e5708984a4bfd4959e714d4a42a40debf6d6d
Zdeno <zdenoslav@gmail.com>
2020-07-11 22:50:26 +0100

    'done'

---
+++ backend/routes/soccerRoutes.js
@@ -0,0 +1,27 @@
+import { addNewPlayer,
+        getPlayers,
+        getPlayerWithID,
+        UpdatePlayer,
+        deletePlayer
+    } from '../controllers/playerControllers';
+
+const routes = (app) => {
+    app.route('/players')
+    //GET endpoint
+    .get(getPlayers)
+
+    // POST endpoint
+    .post(addNewPlayer);
+
+    app.route('/player/:PlayerId')
+
+    //Get specific player
+        .get(getPlayerWithID)
+    //Update a specific player
+        .put(UpdatePlayer)
+
+    //delete a specific player
+        .delete(deletePlayer);
+}
+
+export default routes;
